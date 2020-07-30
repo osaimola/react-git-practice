8eb89e39d91916fe41962281845f29662b04e6aa
Osa <osaimola@gmail.com>
2020-07-30 11:09:30 -0600

    create services

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import About from "./components/About";
+import Services from "./components/Services";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <About />
+        <Services />
       </header>
     </div>
   );
---
+++ src/components/Services.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Services extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Our services</h2>
+        <p>
+          In addition to being some of the coolest devs on plannet earth, we
+          offer the following services:
+        </p>
+
+        <ul>
+          <li>Food tasting</li>
+          <li>Swag fit and coolness testing</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Services;
