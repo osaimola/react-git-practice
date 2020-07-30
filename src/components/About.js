e7f1e52d20a2e9fad2de306671a4b427d9686322
Osa <osaimola@gmail.com>
2020-07-30 11:42:39 -0600

    fix return in about.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,13 @@ import React from "react";
 class About extends React.Component {
   render() {
     return (
-      <h1>Git Branching!</h1>
-      <p>
-        Branches are cool for team work! &nbsp; Especially for preserving the
-        integerity &nbsp; of commit history
-      </p>
+      <>
+        <h1>Git Branching!</h1>
+        <p>
+          Branches are cool for team work! &nbsp; Especially for preserving the
+          integerity &nbsp; of commit history
+        </p>
+      </>
     );
   }
 }
