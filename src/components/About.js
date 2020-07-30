b978aff3bc60169d18b73524e9b95e79caf588d5
Osa <osaimola@gmail.com>
2020-07-30 10:26:45 -0600

    added about section

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <p>
+        Branches are cool for team work! &nbsp; Especially for preserving the
+        integerity &nbsp; of commit history
+      </p>
+    );
+  }
+}
+
+export default About;
