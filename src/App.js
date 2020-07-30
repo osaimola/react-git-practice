19ad690169f2276f722a9ef40dadc770e163d0c1
Osa <osaimola@gmail.com>
2020-07-30 11:01:55 -0600

    fix merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
 import About from "./components/About";
-=======
->>>>>>> master
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <header className="App-header">
         <About />
       </header>
-=======
-      <header className="App-header"></header>
->>>>>>> master
     </div>
   );
 }
