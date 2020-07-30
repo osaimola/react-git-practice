e6b8e9c8c4e2c5a61502ca8e8694b05ae399fdec
Osa <osaimola@gmail.com>
2020-07-30 11:40:35 -0600

    add a heading text to the about

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import React from "react";
 class About extends React.Component {
   render() {
     return (
+      <h1>Git Branching!</h1>
       <p>
         Branches are cool for team work! &nbsp; Especially for preserving the
         integerity &nbsp; of commit history
